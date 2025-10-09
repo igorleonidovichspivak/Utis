7e0921641a8f0c495ddd10e30515d6e1e0fd11ca
Igor Spivak <igor.spivak@gmail.com>
2025-10-09 18:59:13 +0300

    fix api availability

--- tasks-service/Utis.Tasks.WebApi/Program.cs
+++ tasks-service/Utis.Tasks.WebApi/Program.cs
@@ -1,14 +1,15 @@
 using Utis.Tasks.WebApi.Configuration;
 using Serilog;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
 
 
 Log.Logger = new LoggerConfiguration()
@@ -24,7 +25,7 @@ builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName == "Docker")
 {
     app.UseSwagger();
     app.UseSwaggerUI();
@@ -36,6 +37,10 @@ app.UseAuthorization();
 
 //TODO: add tracing middleware with opentel
 //app.UseMiddleware<ExceptionHandlingMiddleware>();
+app.UseRouting();
+app.MapHealthChecks("/health");
+app.MapGet("/myhealth", () => "Service is running!");
+
 
 app.MapControllers();
 
